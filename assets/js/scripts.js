4a054e53c6b8f98e45335f55486d7dc52717ffe3
Daniel <choijungah12@gmail.com>
2015-09-21 22:16:33 -0700

    testing main page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,17 +5,18 @@ jQuery(document).ready(function() {
         Fullscreen background
     */
     $.backstretch("assets/img/backgrounds/1.jpg");
-    
+
     /*
         Form validation
     */
     $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
     });
-    
+
     $('.login-form').on('submit', function(e) {
-    	
+
     	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+			 
     		if( $(this).val() == "" || $(this).val() != yuna) {
     			e.preventDefault();
     			$(this).addClass('input-error');
@@ -26,7 +27,8 @@ jQuery(document).ready(function() {
     		}
 
     	});
-    });
-    
-    
+
+		});
+
+
 });
